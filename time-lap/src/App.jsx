bcd86843a2d74927a06acab1c99a9d57e25d7b17
hongseokpyo <hongseokpyou@gmail.com>
2025-01-03 05:33:41 +0900

    chore

--- time-lap/src/App.jsx
+++ time-lap/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css';
-import AsyncStateExample from './AsyncTest.jsx';
 import Timer from './components/Timer.jsx';
 
 function App() {
   return (
-    <>
-      <div id='app'>
-        <h1>Hello</h1>
-        <Timer />
-      </div>
-      <AsyncStateExample />
-    </>
+    <div id='app'>
+      <h1>Hello</h1>
+      <Timer />
+    </div>
   );
 }
 
